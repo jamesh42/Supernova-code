f83e4f42a786640eb91943a63d088a7583f05179
James Henderson <james_henderson@live.co.uk>
2023-10-22 16:43:20 +0100

    Code for running simulations
    This code runs simulations for various input parameters and fits them with various specified fitting parameters in the usual SNTD way. It then pickles the relevant lightcurve input and fitted parameters so that analysis can be done in future without rerunning the simulations, in separate scripts. A problem with it was that it created data and fitted it in the same loop, and because of the issue with systematic random scaling of the generated lightcurve, the results can be sensitive to variable normalisations of the lightcurves within the loop. The shapes of the lightcurves (and thus the fitted time delays) stay the same, so some of the data, as I understand, should still be useful.

--- resolved_llc.py
+++ resolved_llc.py
@@ -17,8 +17,14 @@ import sntd
 import pickle
 import time
 
+# This is the main code I was using. It saves simulations (the original input parameters, the fitted parameters, and 
+# information about the simulation (i.e. which parameters are fitted)) as pickle files, for other scripts to unpickle
+# and use without worrying about result reproducability or having to spend lots of time rerunning the simulation.
+
+# For timing simulation
 start_time = time.time()
 
+# Parameters
 z_val = 0.3544   # source supernova redshift
 t0_val = 0.
 x1_val = 0.
@@ -29,21 +35,18 @@ nimages = 2  # Multiplicity
 mu_1 = 2
 mu_2 = 7
 
-# Band lists
+# Band lists - for choosing which filters to simulate
 band_list_ztf = ['ztfg','ztfr', 'ztfi']
 band_list_csp = ['cspjd', 'cspyd', 'csphd']
 band_list_full = ['ztfg', 'ztfr', 'ztfi', 'cspjd', 'cspyd', 'csphd']
+# For choosing which magnifications to use
+magnifications_list = [[2,7]]
 
 # Creating zp list in format sntd.createMISN wants
-zp_list = [25.] * len(band_list_full)
+zp_list = [25.] * len(band_list_ztf)
 
 # Time delay list
-delta_t_list = np.linspace(0,40,5)
-delta_t_list = [10,20]
-delta_t_list= [10, 15, 20]
-delta_t_list = [10,20]
-#delta_t_list = [10]
-delta_t_value_list = [10]
+delta_t_value_list = [10] # time delays to simulates
 
 #%%
 
@@ -59,18 +62,14 @@ sn_parameters = {'x1':x1_func,'c':c_func}   # i assume you don't need to specify
 
 # List of models to use and band lists, etc.
 source_model_list = ['salt2-extended', 'hsiao']
-source_model_list = ['salt2-extended']
-#source_model_list = ['salt2-extended']
-band_list_list = [band_list_ztf, band_list_full]
+source_model_list = ['salt2-extended'] # can simulate salt2 or salt2 and hsiao
+band_list_list = [band_list_ztf, band_list_full] #
 band_list_labels = ['ztf', 'both']
 
 # Extinction lists
-a_v_val_list = np.linspace(0.1,0.5, 2)
-a_v_val_list = np.linspace(0.1,0.5,5)
+a_v_val_list = [0.5]   # for simulating multiple extinctions
 
-#a_v_val_list = [0.25]
-#%
-
+# Lists for appending information to during simulation
 a_v_list = []
 delay_residual_list = []
 delta_t_list = []
@@ -78,15 +77,12 @@ source_model_iterated_list = []   # just list to remember each data point's mode
 delay_error_list = []
 mu_1_list = []
 mu_2_list = []
-magnifications_list = [[2,7], [5,5]]
 mag_iteration_list = []
 fitted_mag_list = []
 mag1_err_list = []
 mag2_err_list = []
 
-print('xxx')
-#a_v_val = 0.25
-
+# Starting simulation, which loops over all the values to fit and use in simulation
 for a_v_val in a_v_val_list:
     for mag_list in magnifications_list:
         
@@ -96,11 +92,10 @@ for a_v_val in a_v_val_list:
         for dt_val in delta_t_value_list:
             
             # Creating data  
-            
             dt_1 = -dt_val/2
             dt_2 = dt_val/2
             
-            myMISN = sntd.createMultiplyImagedSN(sourcename='salt2-extended', snType='Ia', redshift=z_val, bands=band_list_full,
+            myMISN = sntd.createMultiplyImagedSN(sourcename='salt2-extended', snType='Ia', redshift=z_val, bands=band_list_ztf,
                          zp=zp_list, cadence=5., epochs=35.,time_delays=[dt_1, dt_2], magnifications=mag_list,
              objectName='My Type Ia SN',telescopename='ztf',av_host=a_v_val, numImages = nimages, sn_params = sn_parameters) 
             myMISN.plot_object()
@@ -116,14 +111,13 @@ for a_v_val in a_v_val_list:
          
             # FITTING DATA
             for source_model in source_model_list:
-                # I am pretty sure here I am just creating the model + data, extracting the data and then recreating the model again, but it's better to be safe and unpack then repack than risk contaminating independent test
-                print(source_model)   # IF I REMOVE THIS, THE CODE BREAKS -- UTTERLY BIZARRE
-                # Fitting again with SNTD
+                # I realise here I am just creating the model + data, extracting the data and then recreating the model again, but it's better to be safe and unpack then repack than risk contaminating independent test
+                print(source_model)   # Inexplicably if I remove this, the code can break. So I just left it in here
                 
                 max_retries = 15
                 retry_count = 0
                 
-                while retry_count < max_retries:
+                while retry_count < max_retries:   # since some runs will not work, but repeating the run can, I try a maximum of 15 times before admitting defeat
                     
                     try:
                         
@@ -132,21 +126,24 @@ for a_v_val in a_v_val_list:
                         
                             fitting_model=sncosmo.Model(source_model,effects=[dust,dust],effect_names=['lens','host'],effect_frames=['free','rest'])    
                         
-                            fitCurves=sntd.fit_data(new_MISN,snType='Ia', models=fitting_model,bands=band_list_full,
+                            fitCurves=sntd.fit_data(new_MISN,snType='Ia', models=fitting_model,bands=band_list_ztf,   # here is where the fitting goes on, choose band list etc. accordingly
                                             params=['x0','x1','t0','c', 'hostebv'],constants={'z':z_val, 'lensebv': 0., 'hostr_v':2.0},
                                             bounds={'t0':(-25,25),'x1':(-2,2),'c':(-1,1), 'hostebv':(0.02,0.333)}, method='parallel',npoints=100)
-                            fitCurves.plot_object(showFit=True)
+                            fitCurves.plot_object(bands='ztfg', showFit=True)
                             plt.show()
                             
+                            # time delay info
                             time_delay_SNTD = fitCurves.parallel.time_delays['image_2'] - fitCurves.parallel.time_delays['image_1']   # image 1 should always be 0!
                             delay_residual = time_delay_SNTD - dt_val
                             
+                            # fitting magnifications info
                             fit_mag_1 = fitCurves.parallel.magnifications['image_1']
                             fit_mag_2 = fitCurves.parallel.magnifications['image_2']
                             
                             fit_mags = [fit_mag_1, fit_mag_2]   # len 2 array with fitted magnifications for image 1 and 2 respectively
                             fitted_mag_list.append(fit_mags)
                             
+                            # storing errors
                             mag_1_errs = fitCurves.parallel.magnification_errors['image_1']   # think this is always 0?
                             mag_2_errs = fitCurves.parallel.magnification_errors['image_2']    
                             mag_1_err = [abs(ele) for ele in mag_1_errs]
@@ -162,8 +159,9 @@ for a_v_val in a_v_val_list:
                             mag1_err_list.append(mag_1_err)
                             mag2_err_list.append(mag_2_err)
                             
+                            # appending info to lists
                             a_v_list.append(a_v_val)
-                            delay_residual_list.append(delay_residual)   #for trying new method, 2 denotes new method
+                            delay_residual_list.append(delay_residual)
                             delta_t_list.append(dt_val)
                             source_model_iterated_list.append(source_model)
                             mag_iteration_list.append(mag_list)   #for input magnifications
@@ -171,12 +169,12 @@ for a_v_val in a_v_val_list:
                             break
                 
                             
-                        else:
+                        else:  # for other source model, requires different specification since salt2 uses x0 and others use 'amplitude'. Besides that this else: section is identical.
                         
                         
                             fitting_model=sncosmo.Model(source_model,effects=[dust,dust],effect_names=['lens','host'],effect_frames=['free','rest'])    
                         
-                            fitCurves=sntd.fit_data(new_MISN,snType='Ia', models=fitting_model,bands=band_list_full,
+                            fitCurves=sntd.fit_data(new_MISN,snType='Ia', models=fitting_model,bands=band_list_ztf,
                                             params=['amplitude','x1','t0','c', 'hostebv'],constants={'z':z_val, 'lensebv': 0., 'hostr_v':2.0},
                                             bounds={'t0':(-25,25),'x1':(-2,2),'c':(-1,1), 'hostebv':(0.02,0.333)}, method='parallel',npoints=100)
                             fitCurves.plot_object(showFit=True)
@@ -188,10 +186,10 @@ for a_v_val in a_v_val_list:
                             fit_mag_1 = fitCurves.parallel.magnifications['image_1']
                             fit_mag_2 = fitCurves.parallel.magnifications['image_2']
                             
-                            fit_mags = [fit_mag_1, fit_mag_2]   # len 2 array with fitted magnifications for image 1 and 2 respectively
+                            fit_mags = [fit_mag_1, fit_mag_2]
                             fitted_mag_list.append(fit_mags)
                             
-                            mag_1_errs = fitCurves.parallel.magnification_errors['image_1']   # think this is always 0?
+                            mag_1_errs = fitCurves.parallel.magnification_errors['image_1']
                             mag_2_errs = fitCurves.parallel.magnification_errors['image_2']    
                             mag_1_err = [abs(ele) for ele in mag_1_errs]
                             mag_2_err = [abs(ele) for ele in mag_2_errs]
@@ -207,7 +205,7 @@ for a_v_val in a_v_val_list:
                             mag2_err_list.append(mag_2_err)
                             
                             a_v_list.append(a_v_val)
-                            delay_residual_list.append(delay_residual)   #for trying new method, 2 denotes new method
+                            delay_residual_list.append(delay_residual)
                             delta_t_list.append(dt_val)
                             source_model_iterated_list.append(source_model)
                             mag_iteration_list.append(mag_list)   #for input magnifications
@@ -215,7 +213,7 @@ for a_v_val in a_v_val_list:
                             break
                             
                     except ZeroDivisionError:
-                        retry_count += 1   # USE ENUMERATE IN THIS 5:21 of python vid
+                        retry_count += 1
                         continue
                     
                     except Exception:
@@ -229,24 +227,37 @@ for a_v_val in a_v_val_list:
                     print(f"Code executed successfully. Took {retry_count + 1} tries.")
                         
         
-           
 end_time = time.time()
 time_taken = end_time - start_time
 
-print(f'Time taken: {time_taken}')        
+print(f'Time taken for simulation: {time_taken}')        
 
 #%%
 
+# Storing information for pickling
 data = Table()
-data['A_v'] = a_v_list # list of times for which we have data
-data['Time delay residual'] = delay_residual_list   # corresponding list of simulated fluxes for each time
-data['Time delay'] = delta_t_list   # applying systematic error
+data['A_v'] = a_v_list
+data['Time delay residual'] = delay_residual_list
+data['Time delay'] = delta_t_list
 data['Source model'] = source_model_iterated_list
 data['Time delay errors'] = delay_error_list
 data['Input magnifications'] = mag_iteration_list
 data['Fitted magnifications'] = fitted_mag_list
 data['Image 1 fitted mag errors'] = mag1_err_list   # this should always just be zero
 data['Image 2 fitted mag errors'] = mag2_err_list
+
+# File path to save the pickle file
+fit_output_file_path = 'Pickle/Full sim tables/misc..pkl' #misc so that data is not overwritten accidentally
+
+# Open the file in binary mode and save the data using pickle
+with open(fit_output_file_path, 'wb') as file:
+    pickle.dump(data, file)
+    
+#%%
+
+# The more functional part of the code is above. This last section was for an earlier plot I was making of the magnification residuals versus extinction.
+# It is probably worth deleting, but I left it in case this test is to be run again. But as mentioned it is more likely that this code is only of further use
+# insofar as it can be used to run simulations and store information to pickle files.
 
 mag_1_inputs = []
 mag_1_fits = []
@@ -259,15 +270,8 @@ for i, mu_input_list in enumerate(mag_iteration_list):
     mag_1_fits.append(fitted_mag_list[i][0])
     mag_2_fits.append(fitted_mag_list[i][1])
 
-
-print(mag_1_fits)
-print(mag_2_fits)
 mag1_residuals = np.array(mag_1_fits) - np.array(mag_1_inputs)
 mag2_residuals = np.array(mag_2_fits) - np.array(mag_2_inputs)
-
-# I believe this ^ is all the key information from this run, besides input parameters and parameters varied, 
-# but I will specify those in the overleaf. This is all the information needed to plot relevant graphs, 
-# so this is what will be pickled and then can be re-plotted in any way desired in the future.
 
 delay_up_errors = []
 delay_down_errors = []
@@ -293,22 +297,11 @@ mu2_down_errors = []
 for i, err in enumerate(mag2_err_list): #for each data point
     mu2_down_errors.append(err[0])
     mu2_up_errors.append(err[1])
-    
-# File path to save the pickle file
-fit_output_file_path = 'Pickle/Full sim tables/misc.pkl'  #specify which one!
-
-# Open the file in binary mode and save the data using pickle
-with open(fit_output_file_path, 'wb') as file:
-    pickle.dump(data, file)
-
-# av_min = a_v_list[0]
-# av_max = a_v_list[-1]
 
 plt.title(r'Time delay residuals for different magnifications ($\Delta t = 10$)')   #fitted only with salt2 (hsiao data not included in this)
 plt.xlabel(r'$A_V$')
 plt.ylabel(r'Output $\mu$ residuals')
-# plt.ylim(-1,1)
-#plt.ylim(-5,0)
+
 for i, A_v in enumerate(np.array(data['A_v'])):   # i want a plot 
     
     y_err_delays = [[delay_down_errors[i]], [delay_up_errors[i]]] # this needs to be in this format, ie errorbar only accepts form [[down_error], [up_error]] for a given point (or of course [[down_errorS], [up_errorS]])
@@ -328,57 +321,12 @@ for i, A_v in enumerate(np.array(data['A_v'])):   # i want a plot
             plt.errorbar(A_v, mag2_residuals[i], yerr = y_err_mu2, c = 'green')
     else:
         pass
+    
 handles, labels = plt.gca().get_legend_handles_labels()
 by_label = dict(zip(labels, handles))
-# Place the legend slightly above the center
 legend_offset = (0.5, 0.9)  # (x, y) coordinates relative to the axes
 plt.legend(by_label.values(), by_label.keys(), loc='upper center', bbox_to_anchor=legend_offset)   #just copied off stack exchange to stop legend repeating itself
 plt.show()
 
-# So red denotes mu1
-
-
-# plt.title(r'Time delay residuals for various fitting models')
-# plt.xlabel(r'Original time delay (days)')
-# plt.ylabel(r'Output time delay residuals (days)')
-# plt.ylim(-2,2)
-# for i, A_v in enumerate(np.array(data['A_v'])):
-    
-#     y_err = [[down_errors[i]], [up_errors[i]]] # this needs to be in this format, ie errorbar only accepts form [[down_error], [up_error]] for a given point (or of course [[down_errorS], [up_errorS]])
-    
-#     if np.array(data['Source model'][i]) == 'salt2-extended': 
-#         plt.scatter(np.array(data['Time delay'][i]), np.array(data['Time delay residual'][i]), marker = 'x', c = A_v, vmin = av_min, vmax = av_max, label = 'salt2-extended')
-#         plt.errorbar(np.array(data['Time delay'][i]), np.array(data['Time delay residual'][i]), yerr = y_err, color=plt.cm.viridis((A_v - av_min) / (av_max - av_min)))
-#     elif np.array(data['Source model'][i]) == 'hsiao': 
-#         plt.scatter(np.array(data['Time delay'][i]), np.array(data['Time delay residual'][i]), marker = 'o', c = A_v, vmin = av_min, vmax = av_max, label = 'hsiao')
-#         plt.errorbar(np.array(data['Time delay'][i]), np.array(data['Time delay residual'][i]), yerr = y_err, color=plt.cm.viridis((A_v - av_min) / (av_max - av_min)))
-
-# plt.colorbar(label=r'$A_V$')
-# handles, labels = plt.gca().get_legend_handles_labels()
-# by_label = dict(zip(labels, handles))
-# plt.legend(by_label.values(), by_label.keys(), loc='upper right')   #just copied off stack exchange to stop legend repeating itself
-# plt.show()
-
-# USE ENUMERATE FOR ALL FUTURE LOOPS
-# ALSO USE f STRINGS FOR ALL LABELS ETC.
-
-# plot lightcurves IN OVERLEAF AS KAISEY SAYS, USEFUL TO SEE, COULD IR BE MISSING THE SECOND PEAK AT LARGE TIME DELAYS SINCE 
-# FINITE TIME SERIES SO DATA IS TRUNCATED CAUSING LOSS OF INFORMATION AND THEREFORE WORSE FIT
-
-
-
-
-#%%
-
-"""
-Questions: 
-    
-    -what is t1_error = fitCurves.images['image_1'].fits.res.errors['t0']? I have no idea what this, and the corresponding image_2 quantity, is
-    - what is relationship between t1_error above and the parallel.time_delay_errors ?
-    
-    -  #t1 = fitCurves.images['image_1'].fits.res.param_dict['t0']   # why  on earth would image_1 not work - it turns out not to exist! Maybe by definition set to 0?
-    - here images.['image_1']..... param dict doesnt exist but it does for image 2, why?
-    
-""" 
 
 
